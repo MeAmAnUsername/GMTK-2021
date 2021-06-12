780ccde0b699e24a268e06924b95837f199b9e94
dkorpel <dkorpel@gmail.com>
2021-06-12 17:58:29 +0200

    add graphics and sound

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -12,43 +12,41 @@ var torque_charge = 0
 var input_x : int
 var input_y : int
 
-var in_range : bool
+var input : bool
 var dead : bool = false
 
 func _init():
-	in_range = true
+	input = true
 	angular_damp = 6.0
 	linear_damp = 0.6
 	gravity_scale = 3.0
-	
 	#$RotorSound.play()
 
 func _ready():
 	# animation playing is disabled on node to prevent merge conflicts
 	#$HeliSprite.play()
-	#$RotorSound.play()
-	#$RotorSound.stream.loop_mode = AudioStreamPlayer2D.LOOP_FORWARD
+	#$RotorSound.playing = true
+	$RotorSound.play(0.0)
 	pass
+	#$RotorSound.stream.loop_mode = AudioStreamPlayer2D.LOOP_FORWARD
 
 func _process(delta):
 	
 	if beaconCount > 0 and ! dead:
+		input = true
 		input_x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 		input_y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
 	else:
+		input = false
 		input_x = 0
 		input_y = 0
 	
-	var any_input = input_x !=0 or input_y !=0
-	#$RotorSound.pitch_scale += delta * (1.0 if in_range else -1.0)
-	#$RotorSound.pitch_scale = clamp($RotorSound.pitch_scale, 0.5, 1.0)
-		
-	#$RotorSound.volume_db += delta * (1.0 if any_input else -1.0)
-	#$RotorSound.volume_db = clamp($RotorSound.volume_db, -100, -3)
+	$RotorSound.pitch_scale += delta * (1.0 if input else -1.0)
+	$RotorSound.pitch_scale = clamp($RotorSound.pitch_scale, 0.25, 0.75)
 	
-	if beaconCount == 0:
-		input_x = 0
-		input_y = 0
+	var has_thrust = input_x !=0 or input_y !=0
+	$RotorSound.volume_db += delta * (10.0 if has_thrust else -10.0)
+	$RotorSound.volume_db = clamp($RotorSound.volume_db, -100, -6)
 	
 	thrust_charge += delta * (1.25 if (input_y == 0) else -8.00)
 	torque_charge += delta * (2.50 if (input_x == 0) else -8.00)
