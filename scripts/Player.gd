e2090acec0f77dfe9e44ea9ea8d4a7671d4c2424
dkorpel <dkorpel@gmail.com>
2021-06-13 18:25:09 +0200

    level tweaks

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -121,7 +121,7 @@ func _integrate_forces(state):
 	# Try to stay upright
 	var angle_dif = (-TAU*0.25 - rotation) / (TAU*0.25)
 	if beaconCount > 0 and (abs(angle_dif) < 1):
-		applied_torque += angle_dif * 4000
+		applied_torque += angle_dif * 4500
 	
 	var total_thrust = thrust_charge * 5000 + thrust
 	applied_force = Vector2(0, total_thrust * input_y).rotated(rotation + TAU*0.25)
